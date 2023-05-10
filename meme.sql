7f4b169d04d45d0e6a4c4455673b4437dccc249b
Your Name <you@example.com>
2023-05-10 14:39:49 +0200

    cos jest

--- meme.sql
+++ meme.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Kwi 2023, 14:15
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.0.19
+-- Generation Time: Maj 02, 2023 at 12:26 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `meme`
+-- Database: `meme`
 --
 
 -- --------------------------------------------------------
@@ -34,10 +34,10 @@ CREATE TABLE `post` (
   `title` text NOT NULL,
   `authorId` int(11) NOT NULL,
   `removed` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `post`
+-- Dumping data for table `post`
 --
 
 INSERT INTO `post` (`id`, `filename`, `timestamp`, `title`, `authorId`, `removed`) VALUES
@@ -56,15 +56,62 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `email`, `password`) VALUES
 (1, 'gowno@chuj.pl', '$argon2i$v=19$m=65536,t=4,p=1$cXgycXFDb3JOUzFDOVZUSg$UBYk/Fw40P4kcAKRjclF34BxVDONTMkHAfsHlLfdqv8'),
 (3, 'olaffajszer@wp.pl', '$argon2i$v=19$m=65536,t=4,p=1$dGtQcXpKRzRCYVpGMkFldQ$JuRIFob3t8o+sWFm8goECNeOk4im9qfVdwqOzyfUPuQ');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `vote`
+--
+
+CREATE TABLE `vote` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vote`
+--
+
+INSERT INTO `vote` (`id`, `post_id`, `score`, `user_id`) VALUES
+(4, 1, 1, 1),
+(5, 1, 1, 1),
+(6, 1, 1, 1),
+(7, 1, 1, 1),
+(8, 1, 1, 1),
+(9, 1, 1, 1),
+(10, 1, 1, 1),
+(11, 1, 1, 1),
+(12, 1, 1, 1),
+(13, 1, 1, 1),
+(14, 2, -1, 1),
+(15, 2, 1, 1),
+(16, 2, 1, 1),
+(17, 2, 1, 1),
+(18, 2, 1, 1),
+(19, 2, 1, 1),
+(20, 2, 1, 1),
+(21, 2, 1, 1),
+(32, 1, -1, 1),
+(33, 1, -1, 1),
+(34, 1, -1, 1),
+(35, 1, -1, 1),
+(36, 1, -1, 1),
+(37, 1, -1, 1),
+(38, 1, -1, 1),
+(39, 1, -1, 1),
+(40, 1, -1, 1),
+(41, 2, 1, 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -83,20 +130,45 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- Indeksy dla tabeli `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vote_ibfx_1` (`post_id`),
+  ADD KEY `vote_ibfx_2` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `post`
+-- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT dla tabeli `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `vote`
+--
+ALTER TABLE `vote`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `vote`
+--
+ALTER TABLE `vote`
+  ADD CONSTRAINT `vote_ibfx_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
+  ADD CONSTRAINT `vote_ibfx_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
